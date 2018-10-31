ada257b5e95324ccea040a503d10986ede7f05c8
Ricky Islam <mdrickyislam.cse@gmail.com>
2018-10-31 14:52:13 +0600

    Add project files.

---
+++ Material Blank/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Material_Blank.MvcApplication" Language="C#" %>
---
+++ Material Blank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Material_Blank.Startup))]
+namespace Material_Blank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
